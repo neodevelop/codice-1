0990ee54d89a03f95d789e8c52eae3ffb8b11907
neodevelop <neodevelop@gmail.com>
2012-03-07 17:22:10 -0600

    Adding plugins
    - Adding base plugins

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,43 +1,40 @@
-grails.servlet.version = "2.5" // Change depending on target container compliance (2.5 or 3.0)
+grails.servlet.version = "2.5"
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
-    inherits("global") {
-        // uncomment to disable ehcache
-        // excludes 'ehcache'
-    }
-    log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-    checksums true // Whether to verify checksums on resolve
+  inherits("global") {
+    // uncomment to disable ehcache
+    // excludes 'ehcache'
+  }
+  log "warn"
+  checksums true
 
-    repositories {
-        inherits true // Whether to inherit repository definitions from plugins
-        grailsPlugins()
-        grailsHome()
-        grailsCentral()
+  repositories {
+    inherits true
+    grailsPlugins()
+    grailsHome()
+    grailsCentral()
+  }
+  dependencies {
+    runtime 'mysql:mysql-connector-java:5.1.16'
+  }
 
-        // uncomment these to enable remote dependency resolution from public Maven repositories
-        //mavenCentral()
-        //mavenLocal()
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
-    }
-    dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-
-        // runtime 'mysql:mysql-connector-java:5.1.16'
-    }
-
-    plugins {
-        compile ":hibernate:$grailsVersion"
-        compile ":jquery:1.6.1.1"
-        compile ":resources:1.0.2"
-
-        build ":tomcat:$grailsVersion"
-    }
+  plugins {
+    compile ":hibernate:$grailsVersion"
+    compile ":jquery:1.6.1.1"
+    compile ":resources:1.1.5"
+    compile ":simple-blog:0.2.1"
+    compile ":taggable:1.0.1"
+    compile ":commentable:0.8.1"
+    compile ":feeds:1.5"
+    compile ":spring-security-core:1.2.7.2"
+    compile ":twitter-bootstrap:2.0.1.19"
+    test ":geb:0.6.3"
+    test ":spock:0.6"
+    
+    build ":tomcat:$grailsVersion"
+  }
 }
